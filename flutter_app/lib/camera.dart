a401ac09168c0dade526c6e812797d41f25467f9
gjurajlatwit <gjurajl@wit.edu>
2023-04-16 18:44:57 -0400

    Made it so that camera opens up right away.

--- flutter_app/lib/camera.dart
+++ flutter_app/lib/camera.dart
@@ -2,8 +2,35 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/camera_page.dart';
 
-class Camera extends StatelessWidget {
+class Camera extends StatefulWidget {
   const Camera({Key? key}) : super(key: key);
+
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  late List<CameraDescription> cameras;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    cameras = await availableCameras();
+    if (cameras.isEmpty) {
+      // No cameras available
+      return;
+    }
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CameraPage(cameras: cameras),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +41,7 @@ class Camera extends StatelessWidget {
         elevation: 0,
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await availableCameras().then(
-              (value) => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CameraPage(cameras: value,),
-                ),
-              ),
-            );
-          },
-          child: const Text('Launch Camera'),
-        ),
+        child: const CircularProgressIndicator(),
       ),
     );
   }
